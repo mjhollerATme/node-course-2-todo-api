10292c3cd7660a2224830be6d4839c3f897e8e96
Jonathan <kontakt@inselpark.com>
2018-12-30 17:11:26 +0100

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
 
-
   var todo = new Todo({
     text: req.body.text
   });
@@ -21,6 +20,14 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000, () => {
