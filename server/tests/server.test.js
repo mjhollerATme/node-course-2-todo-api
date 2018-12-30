23b26c5a0a026238d85122fa66f7bfa3069ef6cf
Jonathan <kontakt@inselpark.com>
2018-12-30 19:23:09 +0100

    added Test for todo/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,20 +1,24 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const dummyTodos = [
   {
+    _id: new ObjectID('5c28f106d777e44a5200b3d3'),
     text: "Something todo"
   },
   {
+    _id: new ObjectID('5c28f106d777e44a5200b3d4'),
     text: "Something else todo"
   },
   {
+    _id: new ObjectID('5c28f106d777e44a5200b3d5'),
     text: "Just another task"
   },
   {
+    _id: new ObjectID('5c28f106d777e44a5200b3d6'),
     text: "A boring task to complete today"
   }
 ];
@@ -85,6 +89,39 @@ describe('GET /todos route', () => {
         expect(res.body.todos.length).toBe(dummyTodos.length);
       })
       .end(done);
+  });
+
+});
+
+describe('GET /todos/:id route', () => {
+
+  it('should return todo by valid id', (done) => {
+    request(app)
+      .get(`/todos/${dummyTodos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(dummyTodos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+
+    var hexID = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexID}`)
+      .expect(404)
+      .end(done);
+
+  });
+
+  it('should return 400 if todo id invalid', (done) => {
+
+    request(app)
+      .get(`/todos/123`)
+      .expect(400)
+      .end(done);
+
   });
 
 });
